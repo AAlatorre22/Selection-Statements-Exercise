010a4355836810e0cc0593a4eb91c969e4fd7e69
Alejandro Alatorre <alejandro.c.alatorre@gmail.com>
2023-03-08 11:32:44 -0800

    Added guessing game

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Let's play a game! I'll think of a number between 1-100 and you try to guess it!");
+            Console.WriteLine("Please enter your guess.");
+
+            var num = 22;
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < 22)
+            {
+                Console.WriteLine("Too low!");
+            }
+
+            else if (userInput > 22)
+            {
+                Console.WriteLine("Too high!");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
         }
     }
 }
